7ef617880ef76c5e1411948e05d5b1804f934bb9
triplestrikee <shenhanaholic@gmail.com>
2018-03-28 14:53:05 -0700

    objects and functions in js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-function greet(name){
-    name = name || "<put your name here>";
-    console.log('Hello ' + name);
+//THREE WAYS OF DEFINING FUNCTION IN JS
+//1. function statement
+function greet(){
+    console.log('Hi');
 }
 
+//2. function expression, functions are objects in JS
+var greet2 = function(){
+    console.log('Hi2');
+}
+
+
 greet();
-greet('Han');
+greet2();
+
+//3. Passing anonymous function, functions are treat as special objects 
+function log(a){
+    console.log();
+}
+
+log(function(){
+    console.log('hi4')
+});
